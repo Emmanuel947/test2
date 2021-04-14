489ce5c35988325e0921f044c8208545939567ef
Emmanuel947 <53540619+Emmanuel947@users.noreply.github.com>
2021-04-14 16:59:54 +0100

    Add files via upload

---
+++ build/precache-manifest.6eb7f2271c5ab62f1d1280e82967e393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e5c6416ae87dfa42950",
+    "url": "/static/css/main.4b7a51e8.chunk.css"
+  },
+  {
+    "revision": "2e5c6416ae87dfa42950",
+    "url": "/static/js/main.2e5c6416.chunk.js"
+  },
+  {
+    "revision": "47fa6c469fe2159d2f45",
+    "url": "/static/css/1.5215b50f.chunk.css"
+  },
+  {
+    "revision": "47fa6c469fe2159d2f45",
+    "url": "/static/js/1.47fa6c46.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e687aee22f7a3c4fbba96e76bbb2562a",
+    "url": "/index.html"
+  }
+];
